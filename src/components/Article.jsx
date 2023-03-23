ae4739cd215cc9465eb6b040f6a67c2ed19f008a
Moroz Tetiana <tania@Morozs-MacBook-Air.local>
2023-03-23 19:32:26 +0200

    Implemented the article component

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,1 +1,34 @@
 // export const Article = () => ();
+import React from "react";
+
+
+const getDateISO = (value) => {
+  console.log(value instanceof Date)
+  console.log(value.toISOString())
+  console.log(value.toISOString().slice(0, 10))
+  return value.toISOString().slice(0, 10);
+}
+
+const getDateLocalString = (value) => {
+  return value.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  })
+}
+
+export const Article = ({ title, date, text }) => {
+  return  <div className="box">
+    <h2 className="title" data-cy="title">
+      {title}
+    </h2>
+
+    <p className="subtitle">
+      <time dateTime={getDateISO(date)} data-cy="date">
+        {getDateLocalString(date)}
+      </time>
+    </p>
+
+    <p data-cy="text">{text}</p>
+  </div>
+}
